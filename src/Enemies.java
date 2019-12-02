042588e7a180612d21a927e7952ae1f126faa8f5
colinmarchbanks <marchbanks.colin@gmail.com>
2019-12-02 12:28:55 -0600

    Added some basic ranged combat, needs balancing and some overhaul to the hero and enemy classes

--- src/Enemies.java
+++ src/Enemies.java
@@ -4,9 +4,11 @@ public class Enemies {
 
     private int Health;
     private int Armor;
-    private int Attack;
-    private static int enemyhp;
-    private static int enemymeleedmg;
+    private int MeleeAttack;
+    private int RangeAttack;
+    private int MovementSpeed;
+    private static int enemyhp; // this might be Health
+    private static int enemymeleedmg; // this might be Attack
     private static int Level;
     private String name;
 
@@ -15,8 +17,8 @@ public class Enemies {
 
 //    int[] enemyHpArray = {1, 9, 19, 24, 32, 40};
 //    int[] enemyMeleedmgArray = {1, 1, 4, 6, 7, 9};
-//    enemyhp = enemyHpArray[Level];
-//    enemymeleedmg = enemyMeleedmgArray[Level];
+//    Health = enemyHpArray[Level];
+//    Attack = enemyMeleedmgArray[Level];
 
     public int getHealth() {
         return this.Health;
@@ -26,13 +28,15 @@ public class Enemies {
         return this.Armor;
     }
 
-    public int getAttack(){
-        return this.Attack;
+    public int getMeleeAttack(){
+        return this.MeleeAttack;
     }
 
     public String getName() {
         return name;
     }
+
+    public int getRangeAttack() { return RangeAttack; }
 
     public void setHealth(int newHealth){
         this.Health = newHealth;
@@ -42,17 +46,31 @@ public class Enemies {
         this.Armor = newArmor;
     }
 
-    public Enemies(int health, int armor, int attackPower, String name){
+    public Enemies(int health, int armor, int meleePower,int rangePower, String name){
         this.Health = health;
         this.Armor = armor;
-        this.Attack = attackPower;
+        this.MeleeAttack = meleePower;
+        this.RangeAttack = rangePower;
         this.name = name;
     }
-    public int[] attack(int health, int armor) {
+    public int[] meleeAttack(int health, int armor) {
         Random rand = new Random();
         // Health and armor is going to equal opposition health and armor.
         // Attack points is going to be whoever is attacking.
-        int hit = rand.nextInt(this.getAttack());
+        int hit = rand.nextInt(this.getMeleeAttack());
+
+        if (armor >= hit) {
+            armor = armor - hit;
+        } else {
+            health -= (hit - armor);
+            armor = 0;
+        }
+        return new int[]{health, armor, hit};
+    }
+
+    public int[] rangeAttack(int health, int armor){
+        Random rand = new Random();
+        int hit = rand.nextInt(this.getRangeAttack());
 
         if (armor >= hit) {
             armor = armor - hit;
